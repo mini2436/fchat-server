52f6b0ea906c6abb7d03d5c6d5ad8ee7bcf0ccf1
mini2436 <584991126@qq.com>
2021-07-15 16:34:23 +0800

    feat(基础功能完善): 增加文件上传支持
    文件上传功能完善

--- fchat-api/src/main/java/xyz/mini2436/fchat/api/api/SystemApi.java
+++ fchat-api/src/main/java/xyz/mini2436/fchat/api/api/SystemApi.java
@@ -1,6 +1,8 @@
 package xyz.mini2436.fchat.api.api;
 
+import cn.hutool.core.util.IdUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -8,6 +10,8 @@ import xyz.mini2436.fchat.api.model.vo.ApiVo;
 import xyz.mini2436.fchat.api.model.vo.SeaweedFsVo;
 import xyz.mini2436.fchat.api.utils.FileUtil;
 import xyz.mini2436.fchat.model.vo.ResultVO;
+
+import java.nio.file.Paths;
 
 /**
  * 系统API
@@ -21,22 +25,30 @@ import xyz.mini2436.fchat.model.vo.ResultVO;
 public class SystemApi extends ApiVo {
     private final FileUtil fileUtil;
 
+    @Value("${fchat.seaweedFs.windowsTempFilePath}")
+    private String windowsTempFilePath;
+
     /**
      * 上传文件
+     *
      * @return 返回文件服务器的fid与文件的访问URL
      */
     @PostMapping("uploadFile")
-    Mono<ResultVO<SeaweedFsVo>> uploadFile(@RequestPart("file") Mono<FilePart> filePart){
-        return fileUtil.uploadFile(filePart).flatMap(this::success);
+    Mono<ResultVO<SeaweedFsVo>> uploadFile(@RequestPart("file") Mono<FilePart> filePart) {
+        String nameId = IdUtil.simpleUUID();
+        filePart.flatMap(it -> it.transferTo(Paths.get(windowsTempFilePath + nameId + "-" + it.filename()))).subscribe();
+        return filePart.flatMap(v -> fileUtil.uploadFile(windowsTempFilePath + nameId + "-" + v.filename()))
+                .flatMap(this::success);
     }
 
     /**
      * 根据文件的Fid获取文件的访问连接
+     *
      * @param fid 需要获取的文件唯一标识
      * @return 返回文件服务器查询到的文件数据
      */
     @GetMapping("fileByFid/{fid}")
-    Mono<SeaweedFsVo> getFileByFid(@PathVariable("fid") Mono<String> fid){
+    Mono<SeaweedFsVo> getFileByFid(@PathVariable("fid") Mono<String> fid) {
         return null;
     }
 
--- fchat-api/src/main/java/xyz/mini2436/fchat/api/utils/FileUtil.java
+++ fchat-api/src/main/java/xyz/mini2436/fchat/api/utils/FileUtil.java
@@ -1,23 +1,18 @@
 package xyz.mini2436.fchat.api.utils;
 
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.StrUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import xyz.mini2436.fchat.api.model.bo.SeaweedFs;
 import xyz.mini2436.fchat.api.model.bo.SeaweedFsUpload;
 import xyz.mini2436.fchat.api.model.vo.SeaweedFsVo;
-
-import java.io.File;
 
 /**
  * 文件处理工具类
@@ -27,6 +22,7 @@ import java.io.File;
  **/
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class FileUtil {
     @Value("${fchat.seaweedFs.host}")
     private String seaweedFsHost;
@@ -40,13 +36,14 @@ public class FileUtil {
     @Value("${fchat.seaweedFs.linuxTempFilePath}")
     private String linuxTempFilePath;
 
+    private WebClient webClient = WebClient.create();
+
     /**
      * 获取文件服务器的存放Fid
      *
      * @return 返回文件服务器的请求数据
      */
     public Mono<SeaweedFs> getFid() {
-        WebClient webClient = WebClient.create();
         return webClient.get()
                 .uri("http://" + seaweedFsHost + ":" +seaweedFsPort + "/dir/assign")
                 .retrieve()
@@ -56,37 +53,16 @@ public class FileUtil {
     /**
      * 文件上传至文件服务器
      *
-     * @param filePart 上传的文件
+     * @param filePath 上传的文件
      * @return 返回上后的Fid以及文件访问URL地址
      */
-    public Mono<SeaweedFsVo> uploadFile(Mono<FilePart> filePart) {
-        // 保存到临时文件
-        return filePart.map(file -> {
-            File localUploadFile = new File(System.getProperty("os.name").toUpperCase().startsWith("WIN")
-                    ? windowsTempFilePath + IdUtil.simpleUUID() + file.filename()
-                    : linuxTempFilePath + IdUtil.simpleUUID() + file.filename());
-            file.transferTo(localUploadFile);
-            return localUploadFile;
-        }).flatMap(localUploadFile -> {
-            FileSystemResource resource = new FileSystemResource(localUploadFile);
-            // 封装请求参数
-            MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
-            param.add("myFile", resource);
-            // 将临时文件上传到文件服务器
-            Mono<SeaweedFs> fid = getFid().defaultIfEmpty(SeaweedFs.builder().build());
-            WebClient webClient = WebClient.create();
-            return fid.flatMap(seaweedFs ->
-                    webClient.post()
-                    .uri("http://" + seaweedFs.getPublicUrl() + "/" + seaweedFs.getFid())
-                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                    .body(BodyInserters.fromMultipartData(param))
-                    .retrieve()
-                    .bodyToMono(SeaweedFsUpload.class))
-                    .defaultIfEmpty(SeaweedFsUpload.builder().build())
-                    .map(seaweedFsUpload -> StrUtil.isBlank(seaweedFsUpload.getETag())
-                            ? SeaweedFsVo.builder().build()
-                            : SeaweedFsVo.builder().fid(fid.block().getFid()).url(fid.block().getPublicUrl()+"/"+fid.block().getFid()).build()
-                    );
-        });
+    public Mono<SeaweedFsVo> uploadFile(String filePath) {
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("file", new FileSystemResource(filePath));
+        return getFid().flatMap(fid -> webClient.post().uri("http://"+fid.getPublicUrl()+"/"+fid.getFid()).contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(builder.build()))
+                .retrieve()
+                .bodyToMono(SeaweedFsUpload.class)
+                .map(seaweedFsUpload -> SeaweedFsVo.builder().fid(fid.getFid()).url("http://"+fid.getPublicUrl()+"/"+fid.getFid()).build()));
     }
 }
